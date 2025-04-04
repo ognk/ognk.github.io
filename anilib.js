aa2bb3ebd4c461bf6c002cf76f49bcfbd04c174d
ognk <86823528+ognk@users.noreply.github.com>
2025-04-04 15:25:48 +0700

    Update anilib.js

--- anilib.js
+++ anilib.js
@@ -1,108 +1,66 @@
 (function () {
     'use strict';
 
-    // Конфигурация
     const ANIMELIB_URL = 'https://animelib.me';
     const MENU_ICON = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#FF6B6B">
         <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/>
     </svg>`;
 
-    // Главный компонент
-    class AnimelibComponent {
-        constructor(params) {
-            this.params = params;
-            this.initComponents();
+    class AnimelibMain {
+        constructor() {
+            this.html = $('<div class="animelib-main"></div>');
+            this.initBrowser();
         }
 
-        initComponents() {
-            this.scroll = new Lampa.Scroll({mask: true});
-            this.html = $('<div class="animelib-container"></div>');
-            this.menu = this.createMenu();
+        initBrowser() {
+            this.browserConfig = {
+                url: ANIMELIB_URL,
+                title: 'Animelib.me',
+                width: '90%',
+                height: '80%',
+                onBack: () => this.destroy()
+            };
         }
 
-        createMenu() {
-            return $(`
-                <div class="animelib-menu">
-                    <div class="selector" data-action="browser">
-                        <div class="animelib-ico">${MENU_ICON}</div>
-                        <div class="animelib-text">Открыть браузер</div>
-                    </div>
-                </div>
-            `);
-        }
-
+        // Обязательные методы компонента
         start() {
-            this.bindEvents();
-            this.html.append(this.menu);
-            this.html.append(this.scroll.render());
+            Lampa.Browser.show(this.browserConfig);
             return this.html;
         }
 
-        bindEvents() {
-            this.menu.find('[data-action="browser"]').on('hover:enter', () => {
-                Lampa.Browser.show({
-                    url: ANIMELIB_URL,
-                    title: 'Animelib.me',
-                    width: '90%',
-                    height: '80%',
-                    template: 'default',
-                    onBack: () => this.handleBack()
-                });
-            });
-        }
-
-        handleBack() {
-            Lampa.Controller.toggle('content');
-            this.destroy();
-        }
-
-        pause() {}
+        pause() {} // Фикс ошибки component.pause
         stop() {}
         destroy() {
-            this.scroll.destroy();
+            Lampa.Browser.hide();
             this.html.remove();
         }
     }
 
-    // Инициализация плагина
     function initPlugin() {
-        // Регистрация компонента
-        Lampa.Component.add('Animelib', AnimelibComponent);
-
-        // Добавление в главное меню
+        // Регистрация в главном меню
         Lampa.MenuMain.add('animelib', {
             name: 'Animelib',
-            component: AnimelibComponent,
-            params: {},
-            position: 5
+            component: AnimelibMain,
+            icon: MENU_ICON,
+            position: 3, // Позиция после Shikimori
+            onReady: (component) => component.start()
         });
 
-        // Стилизация
+        // Стили для интеграции
         Lampa.Template.add('Animelib-Styles', `
-            .animelib-container {
-                padding: 20px;
-                background: rgba(0, 0, 0, 0.95);
+            [data-menu="animelib"] {
+                color: #FF6B6B !important;
             }
-            .animelib-menu {
-                margin-bottom: 30px;
-            }
-            .animelib-ico {
-                width: 24px;
-                height: 24px;
-                margin-right: 15px;
-            }
-            .animelib-text {
-                font-size: 1.2em;
-                color: #FF6B6B;
+            [data-menu="animelib"] .menu-main__ico {
+                background: rgba(255,107,107,0.1);
             }
         `);
 
         $('body').append(Lampa.Template.get('Animelib-Styles'));
     }
 
-    // Запуск
-    if (!window.animelib_plugin) {
+    if (!window.animelib_fixed) {
         initPlugin();
-        window.animelib_plugin = true;
+        window.animelib_fixed = true;
     }
 })();
